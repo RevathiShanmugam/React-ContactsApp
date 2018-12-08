63cccff6e8268acdae97161f46609aa8277ac5c8
Revathi Shanmugam <srevathikarthik@gmail.com>
2018-12-08 14:38:22 -0800

    Adding avathars to list items

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -6,7 +6,11 @@ class ListContacts extends Component{
                 <ol className='contact-list'>
                     {/*contacts array so we can use map() function*/}
                     {this.props.contacts.map(contact =>
-                        <li key={contact.id}> {contact.name} </li>)
+                        <li key={contact.id} className='contact-list-item'>
+                            <div className='contact-avatar' style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}/>
+                        </li>)
                     }
 
                 </ol>
